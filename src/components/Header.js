f4d4437b4288c8daf26a2448f05126e0e4df380b
Lady Stock <nikki.castlea@gmail.com>
2020-08-22 13:55:12 -0600

    components folder added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className = "header">
+            <hi>Employee Directory</hi>
+            <p>Click on column heading to filter or enter a search term in the box.</p>
+        </div>
+    )
+}
+
+export default Header;
